a9bca335fe4da98ed1a7e77138118ed392bb993d
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-07-09 17:24:07 +0530

    [Mayur] Add : Welcome message added.

---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Product Review Management using LINQ!");
+        }
+    }
+}
